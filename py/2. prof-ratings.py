ae2cecd9f93b667d7b248c0590f8081086b61be2
mingli202 <vincentmingli@gmail.com>
2024-12-25 22:55:54 -0500

    to be removed after refac

--- py/2. prof-ratings.py
+++ py/2. prof-ratings.py
@@ -20,12 +20,14 @@ def writeProfs():
         print(index)
 
         try:
-            professors.add(i["lecture"]["prof"].replace(
-                "\u00e9", "e").replace("\u00e8", "e"))
+            professors.add(
+                i["lecture"]["prof"].replace("\u00e9", "e").replace("\u00e8", "e")
+            )
 
             if "prof" in i["lab"]:
-                professors.add(i["lab"]["prof"].replace(
-                    "\u00e9", "e").replace("\u00e8", "e"))
+                professors.add(
+                    i["lab"]["prof"].replace("\u00e9", "e").replace("\u00e8", "e")
+                )
         except:
             print(f"error id: {i['count']}")
             exit()
@@ -113,7 +115,6 @@ def getRating(i: str):
             continue
 
         try:
-
             legacyId = re.findall(r'"legacyId":(\d+)', r.text)
 
             for id in legacyId:
@@ -132,34 +133,52 @@ def getRating(i: str):
                     raise
 
                 firstName = stripAccent(
-                    soup.find("div", class_="NameTitle__Name-dowf0z-0 cfjPUG").span.text.strip())
-                lastName = stripAccent(soup.find(
-                    "span", class_="NameTitle__LastNameWrapper-dowf0z-2 glXOHH").text.strip())
+                    soup.find(
+                        "div", class_="NameTitle__Name-dowf0z-0 cfjPUG"
+                    ).span.text.strip()
+                )
+                lastName = stripAccent(
+                    soup.find(
+                        "span", class_="NameTitle__LastNameWrapper-dowf0z-2 glXOHH"
+                    ).text.strip()
+                )
 
                 if len(legacyId) > 1 and lastName != lname and firstName != fname:
                     continue
 
                 try:
-                    avg = float(soup.find(
-                        "div", class_="RatingValue__Numerator-qw8sqy-2 liyUjw").text)
+                    avg = float(
+                        soup.find(
+                            "div", class_="RatingValue__Numerator-qw8sqy-2 liyUjw"
+                        ).text
+                    )
                 finally:
                     pass
 
                 try:
-                    nRating = float(soup.find(
-                        "div", class_="RatingValue__NumRatings-qw8sqy-0 jMkisx").div.a.text.replace("ratings", ""))
+                    nRating = float(
+                        soup.find(
+                            "div", class_="RatingValue__NumRatings-qw8sqy-0 jMkisx"
+                        ).div.a.text.replace("ratings", "")
+                    )
                 finally:
                     pass
 
                 try:
-                    takeAgain = float(soup.find_all(
-                        "div", class_="FeedbackItem__FeedbackNumber-uof32n-1 kkESWs")[0].text.replace("%", ""))
+                    takeAgain = float(
+                        soup.find_all(
+                            "div", class_="FeedbackItem__FeedbackNumber-uof32n-1 kkESWs"
+                        )[0].text.replace("%", "")
+                    )
                 finally:
                     pass
 
                 try:
-                    difficulty = float(soup.find_all(
-                        "div", class_="FeedbackItem__FeedbackNumber-uof32n-1 kkESWs")[1].text)
+                    difficulty = float(
+                        soup.find_all(
+                            "div", class_="FeedbackItem__FeedbackNumber-uof32n-1 kkESWs"
+                        )[1].text
+                    )
                 finally:
                     pass
 
@@ -175,7 +194,7 @@ def getRating(i: str):
 
     # score only for those that exist on the website
     if avg != 0 and nRating != 0:
-        '''
+        """
         Calculated to take into account the number of raters.
         You add one 5/5 and one 0/5 to the rating to obtain the score.
         If there are more raters, the 0/5 will have a lesser impact.
@@ -185,20 +204,22 @@ def getRating(i: str):
           Kazuo Takei, Luiz has 4.6/5 rating with 11 raters. His score is 85.5
 
         More raters makes the rating stronger and accurate.
-        '''
+        """
         score = round((((avg * nRating) + 5) / (nRating + 2)) * 100 / 5, 1)
 
     # print(score)
 
-    ratings.append({
-        "prof": i,
-        "score": score,
-        "avg": avg,
-        "nRating": nRating,
-        "takeAgain": takeAgain,
-        "difficulty": difficulty,
-        "status": status,
-    })
+    ratings.append(
+        {
+            "prof": i,
+            "score": score,
+            "avg": avg,
+            "nRating": nRating,
+            "takeAgain": takeAgain,
+            "difficulty": difficulty,
+            "status": status,
+        }
+    )
 
 
 def main():
