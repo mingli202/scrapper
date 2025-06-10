cbbfb056fb27012733afebd600926b00c3b331bf
mingli202 <vincentmingli@gmail.com>
2025-06-09 22:35:18 -0400

    helper function

--- src/files.py
+++ src/files.py
@@ -1,5 +1,9 @@
 import os
 
+from pydantic_core import from_json
+
+from models import Rating
+
 
 class Files:
     def __init__(self) -> None:
@@ -16,3 +20,24 @@ class Files:
         self.professors = semesterDir + "-professors.json"
         self.ratings = semesterDir + "-ratings.json"
         self.pids = semesterDir + "-pids.json"
+        self.missingPids = semesterDir + "-missing-pid.json"
+
+    def get_raw_file_content(self) -> list[str]:
+        with open(self.rawFile, "r") as file:
+            return from_json(file.read())
+
+    def get_ratings_file_content(self) -> dict[str, Rating]:
+        with open(self.ratings, "r") as file:
+            return {k: Rating(**v) for k, v in from_json(file.read()).items()}
+
+    def get_missing_pids_file_content(self) -> dict[str, str]:
+        with open(self.missingPids, "r") as file:
+            return from_json(file.read())
+
+    def get_professors_file_content(self) -> list[str]:
+        with open(self.professors, "r") as file:
+            return from_json(file.read())
+
+    def get_pids_file_content(self) -> dict[str, str]:
+        with open(self.pids, "r") as file:
+            return from_json(file.read())
